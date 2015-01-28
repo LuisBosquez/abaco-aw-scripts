cfb8d5af53a038dd12ecfb5978816ddb4251ede9
LuisBosquez <luis.bosquez@abacometrics.com>
2015-01-28 16:31:39 -0600

    Updates to source code.
    Finished Bid Adjustment and High Conversion Terms

--- v2/High Conversion Search Terms.js
+++ v2/High Conversion Search Terms.js
@@ -1,71 +1,127 @@
-function main() {  
-  var report = AdWordsApp.report(
-   "SELECT AdGroupName, CampaignName, Query,MatchType, Clicks,Cost, AverageCpc,Ctr,ConversionRate,CostPerConversion,Conversions"+
-   " FROM SEARCH_QUERY_PERFORMANCE_REPORT " +
-   " WHERE " +
-       " Conversions > 1" +
-       " AND Impressions > " + 1 +
-       " AND AverageCpc > " + 1 +
-   " DURING LAST_MONTH");
-  var rows = report.rows();
-  
-  var st_brn_ss = SpreadsheetApp.create("Search Terms: BRN - " + new Date().toDateString());
-  var st_grc_ss = SpreadsheetApp.create("Search Terms: GRC - " + new Date().toDateString());
-  st_brn_ss.appendRow(columns);
-g  var columns = ["AdGroupName", "CampaignName", "Query", "MatchType", "Clicks","Cost", "AverageCpc","Ctr","ConversionRate","CostPerConversion","Conversions"];
-  st_grc_ss.appendRow(columns);
-  
-  while(rows.hasNext())
-  {
-    var row = rows.next();
-    var st_row = [row['AdGroupName'],row['CampaignName'],row['Query'], row['MatchType'], row['Clicks'],row['Cost'],row['AverageCpc'] ,row['Ctr'],row['ConversionRate'],row['CostPerConversion'],row['Conversions']];
-    //Logger.log(row['CampaignName'] + "/" + row['AdGroupName']);
-    if(row['Query'].search(/i[n,m]ge[n,m][e,es]/i) > -1)
-    {      
-       if(isNotAnExistingKeyword(row['Query'], row['MatchType']))
-       {
-         st_brn_ss.appendRow(st_row);
-       }
-    }
-    else
-    {
-      if(isNotAnExistingKeyword(row['Query'], row['MatchType']))
-      {
-        st_grc_ss.appendRow(st_row);
-      }
-    }
-  }
-  
-  st_brn_ss.sort(8,false);
-  st_grc_ss.sort(8,false);
-  
-  Logger.log(st_brn_ss.getName());
-  Logger.log(st_brn_ss.getUrl());
-  Logger.log(st_grc_ss.getName());
-  Logger.log(st_grc_ss.getUrl());
+var ACCOUNT_NAMES = [
+	"INAMI","FONART","SEP","EnTuCine","Weber","CALZADO ANDREA","UTEL","Ingenes","Viajamex","YS Media - Banamex",
+	"CEMAC","ISDI - México","Casas Ara - Abaco","DOOPLA","Amex CM","Amex GCP"];
+var ACCOUNT_TO_EMAILS = {
+	"INAMI":["mariana@abacometrics.com","pamela@abacodigital.com","salvador@abacodigital.com","luis.bosquez@abacometrics.com"],
+	"FONART":["mariana@abacometrics.com","pamela@abacodigital.com","salvador@abacodigital.com","luis.bosquez@abacometrics.com"],
+	"SEP":["mariana@abacometrics.com","pamela@abacodigital.com","salvador@abacodigital.com","luis.bosquez@abacometrics.com"],
+	"EnTuCine":["mariana@abacometrics.com","pamela@abacodigital.com","salvador@abacodigital.com","luis.bosquez@abacometrics.com"],
+	"Weber":["mariana@abacometrics.com","pamela@abacodigital.com","salvador@abacodigital.com","luis.bosquez@abacometrics.com"],
+	"CALZADO ANDREA":["daniel@abacometrics.com","omar@abacodigital.com","salvador@abacodigital.com","luis.bosquez@abacometrics.com"],
+	"UTEL":["andrea@abacodigital.com","karina@abacodigital.com","eric@abacodigital.com","salvador@abacodigital.com","luis.bosquez@abacometrics.com"],
+	"Ingenes":["christian@abacodigital.com","karina@abacodigital.com","salvador@abacodigital.com","luis.bosquez@abacometrics.com"],
+	"Viajamex":["christian@abacodigital.com","daniela@abacodigital.com","salvador@abacodigital.com","luis.bosquez@abacometrics.com"],
+	"YS Media - Banamex":["christian@abacodigital.com","karina@abacodigital.com","eric@abacodigital.com","salvador@abacodigital.com","luis.bosquez@abacometrics.com"],
+	"CEMAC":["christian@abacodigital.com","carla@abacodigital.com","salvador@abacodigital.com","luis.bosquez@abacometrics.com"],
+	"ISDI - México":["omar@abacodigital.com","salvador@abacodigital.com","luis.bosquez@abacometrics.com"],
+	"Casas Ara - Abaco":["christian@abacodigital.com","daniela@abacodigital.com","salvador@abacodigital.com","luis.bosquez@abacometrics.com"],
+	"DOOPLA":["carla@abacodigital.com","andrea@abacodigital.com","salvador@abacodigital.com","luis.bosquez@abacometrics.com"],
+	"Amex CM":["carla@abacodigital.com","andrea@abacodigital.com","salvador@abacodigital.com","luis.bosquez@abacometrics.com"],
+	"Amex GCP":["carla@abacodigital.com","andrea@abacodigital.com","salvador@abacodigital.com","luis.bosquez@abacometrics.com"]
 }
 
-function isNotAnExistingKeyword(searchTerm, matchType)
+var MIN_CONVERSIONS = 1;
+var MIN_IMPRESSIONS = 1;
+var MIN_AVG_CPC = 1;
+
+function main() 
+{  
+	var mccAccount = AdWordsApp.currentAccount();
+  
+	for(var i = 0; i<ACCOUNT_NAMES.length; i++)
+	{ 
+		var accounts = MccApp.accounts()
+	    .withCondition("Name STARTS_WITH '" + ACCOUNT_NAMES[i] + "'")
+        .withCondition('Impressions > 1')
+        .forDateRange("LAST_WEEK")
+    	.get();
+
+	    while(accounts.hasNext())
+    	{
+			var account = accounts.next();
+            Logger.log("ACCOUNT: " + account.getName());
+			MccApp.select(account);
+			
+			var highConversionSearchTerms = getHighConversionSearchTerms();
+			var spreadsheet;
+			if(highConversionSearchTerms.length > 0)
+			{
+				spreadsheet = exportSearchTerms(account, highConversionSearchTerms);
+			}
+			
+			if(MailApp.getRemainingDailyQuota() > 1)
+            {
+            	sendEmailNotification(account, spreadsheet, ACCOUNT_TO_EMAILS[ACCOUNT_NAMES[i]]);
+            }
+			
+    	}
+	}
+    MccApp.select(mccAccount);
+}
+
+function getHighConversionSearchTerms()
+{
+	var highConversionSearchTerms = [];
+	var report = AdWordsApp.report(
+		"SELECT AdGroupName, CampaignName, Query,MatchType, Clicks,Cost, AverageCpc,Ctr,ConversionRate,CostPerConversion,Conversions"+
+		" FROM SEARCH_QUERY_PERFORMANCE_REPORT " +
+		" WHERE " +
+		   " Conversions > " + MIN_CONVERSIONS +
+		   " AND Impressions > " + MIN_IMPRESSIONS +
+		   " AND AverageCpc > " + MIN_AVG_CPC +
+		" DURING LAST_MONTH");
+    var rows = report.rows();
+    while(rows.hasNext())
+  	{
+  		var row = rows.next();
+  		if(isExistingKeyword(row['Query'], row['MatchType']))
+		{
+			continue;
+		}
+		
+		highConversionSearchTerms.push([row['AdGroupName'],row['CampaignName'],row['Query'], row['MatchType'], row['Clicks'],row['Cost'],row['AverageCpc'] ,row['Ctr'],row['ConversionRate'],row['CostPerConversion'],row['Conversions']]);
+  	}
+  	
+  	return highConversionSearchTerms;
+}
+
+function exportSearchTerms(account, searchTerms)
+{
+	var spreadsheet = SpreadsheetApp.create(account.getName() + ": Search Terms - " + new Date().toDateString());
+	var columns = ["AdGroupName", "CampaignName", "Query", "MatchType", "Clicks","Cost", "AverageCpc","Ctr","ConversionRate","CostPerConversion","Conversions"];
+ 	spreadsheet.appendRow(columns); 
+
+    DriveApp.getFileById(spreadsheet.getId()).setSharing(DriveApp.Access.ANYONE, DriveApp.Permission.VIEW);
+    
+    for(var i = 0; i<searchTerms.length; i++)
+    {
+    	spreadsheet.appendRow(searchTerms[i]);
+    }
+    spreadsheet.sort(11, false);
+    return spreadsheet;
+}
+
+function isExistingKeyword(searchTerm, matchType)
 {
   var keywords = AdWordsApp.keywords()
-    .withCondition("Impressions > 1")
+  	.withCondition("Impressions > 1")
     .withCondition("Text CONTAINS_IGNORE_CASE '" + searchTerm + "'")
     .orderBy("Text ASC")
     .forDateRange("LAST_MONTH")
     .get();
+    
   while(keywords.hasNext())
   {
      var keyword = keywords.next();
      var keywordText = keyword.getText().replace(/[\[,\],",+]/g,"");
-    Logger.log("kw:" + keywordText + " mt:" + keyword.getMatchType() + "campaign/adgroup: " + keyword.getCampaign().getName() + "/" + keyword.getAdGroup().getName() + " || st:" + searchTerm + " mt:" + matchType);
-     if(searchTerm.toLowerCase().localeCompare(keywordText.toLowerCase()) == 0 && matchType.toLowerCase().localeCompare(keyword.getMatchType().toLowerCase()) == 0)
+     var keywordTextValue = keywordText.replace("(close variant)","");
+     searchTerm = searchTerm.replace("(close variant)","").trim();
+     if(searchTerm.toLowerCase().localeCompare(keywordTextValue.toLowerCase()) == 0 && matchType.toLowerCase().localeCompare(keyword.getMatchType().toLowerCase()) == 0)
      { 
-       Logger.log("Existing keyword + match type combination");
-       return false;
+       return true;
      } 
   }
-  Logger.log("non existant keyword + match combination. st:" + searchTerm + " mt:" + matchType);
-  return true;
+  return false;
 }
 
 function createKeyword(term)
@@ -84,31 +140,21 @@ function sendEmailNotification(account, data, emailAddresses)
 {
 	var SUBJECT = account.getName() + ": Reporte de Search Terms con altas conversiones. " + _getDateString();
     var recipients = "";
-	var EMAIL_BODY = "Hola, \nSe encontraron los siguientes Search Terms : '" + BROKEN_TAG_TEXT + "'\n";
+	var EMAIL_BODY = "Hola, \nEn las siguientes Spreadsheets se encuentran los Search Terms, divididos entre Branded y no-Branded, para lo cuales \n";
 	for(var i = 0; i<emailAddresses.length; i++)
-	{
-		for(var j = 0; j<data.length; j++) 
-		{
-			EMAIL_BODY += "id: " + data[j].id 
-				+ "\t" + "campaignName: " + data[j].campaignName 
-				+ "\t" + "adGroupName: " + data[j].adGroupName 
-				+ "\t" + "headline: " + data[j].headline 
-				+ "\t" + "description1: " + data[j].description1 
-				+ "\t" + "description2: " + data[j].description2 
-				+ "\t" + "url: " + data[j].url 
-				+ "\t" + "response_code: " + data[j].response_code 
-				+ "\t" + "time: " + _getFullDateString()
-                + "\n";
-	    }
+	{	
     	recipients += emailAddresses[i];
         if(emailAddresses.length > 1 && i < (emailAddresses.length - 1))
         {
            recipients += ",";
         }
 	}
+	EMAIL_BODY += "\n" + data.getName();
+	EMAIL_BODY += "\n" + data.getUrl();
+	EMAIL_BODY += "\n";
     EMAIL_BODY += "\nSaludos.";
     EMAIL_BODY += "\n-Robot de Adwords de Ábaco";
-    MailApp.sendEmail(recipients, SUBJECT, EMAIL_BODY);
+    MailApp.sendEmail("luis.bosquez@abacometrics.com", SUBJECT, EMAIL_BODY);
 }
 
 function _getDateString() {
